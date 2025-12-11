39f8634c30a6140524c1a465532c291988c0ae7e
UnityMergeConfig <jkorondo@gmail.com>
2025-12-11 07:48:50 -0500

    Set Bottles as prefabs. Removed Unnecessary Bottles

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
     private Camera playerCamera;
-    [SerializeField] private float interactDistance = 3f;   
+    [SerializeField] private float interactDistance = 3f;  
+    [SerializeField] private LayerMask mask; 
 
     //Start is called before the first frame update
     void Start()
@@ -16,5 +17,18 @@ public class PlayerInteract : MonoBehaviour
         //create a ray from the center of the camera, shooting outwards.
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.red);
+        if (physics.Raycast(ray, out RaycastHit hitInfo, interactDistance, mask))
+        {
+            //Check if the object we hit has an Interactable component
+            Interactable interactable = hitInfo.collider.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                //If the player presses the E key, interact with the object
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.Interact();
+                }
+            }
+        }
     }
 }
